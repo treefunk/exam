3ee2de716b5e165dfbbed65c8c11c45ca3da32fa
Unknown <jhondeediaz@gmail.com>
2018-03-24 11:09:26 +0800

    classroom create form

--- application/controllers/Classrooms.php
+++ application/controllers/Classrooms.php
@@ -6,24 +6,22 @@ class Classrooms extends BaseController{
     public function __construct()
     {
         parent::__construct();
+        $this->load->model('classroom_model');
     }
 
     public function create()
     {
+        $this->load->helper('form');
         $this->load->library('form_validation');
         //Validations
-        $this->form_validation->set_rules([
-            'field' => 'name',
-            'label' => 'Name',
-            'rules' => 'required|min_length[2]'
-        ]);
+        $this->form_validation->set_rules('name','Name','required|min_length[3]');
        
 
-        if($this->form_validation->run() === FALSE)
+        if($this->form_validation->run() == FALSE)
         {
             $this->wrapper('classrooms/create');
         }else{
- 
+            $this->classroom_model->create();
         }
     }
 }
