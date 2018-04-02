6da71a53813bf5e2282ff40b3043c809f2d78c5f
Unknown <jhondeediaz@gmail.com>
2018-04-02 23:24:50 +0800

    oyoyoyoy

--- application/views/teacher_dashboard.php
+++ application/views/teacher_dashboard.php
@@ -7,11 +7,15 @@ Session Vars
 dba may dndagdag tayo ngayon etong tntype ko
 pag balik sa gitkraken
 di pa to nkasave diba try save
+oy joanna
+
 <h3>Hi <?=$this->session->userdata('username')?>!</h3>
 <h4><?=$this->session->userdata('type')?></h4>
 
 
 JOANNA MARIE TADEO NICOLAS
+
+oy bigti na tayo joanna ^_^
 
 <?php if($this->session->userdata('message')): ?>
 <?=$this->session->userdata('message')?>
