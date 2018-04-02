35eca0a8c71ff00466a12e2dccd1cfcb6c07b3f9
Unknown <juanamariatadeo@gmail.com>
2018-04-02 23:06:03 +0800

    updated header footer

--- application/views/partials/footer.php
+++ application/views/partials/footer.php
@@ -1,4 +1,21 @@
-</body>
-<footer>
+	  <div class="footer">
+			<div class="container">
+					
+				</div>
+				<div class="row footer_bottom">
+				    <div class="copy">
+			           <p>© 2014 Template by w3layouts</p>
+		            </div>
+   				</div>
+			</div>
+		</div>
 
-</footer>
+		    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
